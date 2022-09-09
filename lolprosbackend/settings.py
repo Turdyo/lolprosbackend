a931662d0b9c953f061c13ba1910125d17e10fcd
Martin Gayet <63463668+Turdyo@users.noreply.github.com>
2022-09-09 10:18:22 +0200

    Update settings.py

--- lolprosbackend/settings.py
+++ lolprosbackend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECURITY_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["api.4esport.fr"]
 
 
 # Application definition
