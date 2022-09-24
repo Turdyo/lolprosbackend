675b26dd6fd9934457100a6efd0f59ebcc934cc0
Martin Gayet <63463668+Turdyo@users.noreply.github.com>
2022-09-24 19:35:55 +0200

    Update settings.py

--- lolprosbackend/settings.py
+++ lolprosbackend/settings.py
@@ -154,7 +154,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "lolpros/static/lolpros"
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles-cdn"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
